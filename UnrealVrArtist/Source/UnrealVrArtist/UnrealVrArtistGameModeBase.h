c4457e86be63e7144c5b0d4104ab245b0b6c2eba
JaredP94 <jaredping@yahoo.com>
2019-09-09 21:11:24 +0200

    Project creation

---
+++ UnrealVrArtist/Source/UnrealVrArtist/UnrealVrArtistGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealVrArtistGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALVRARTIST_API AUnrealVrArtistGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
