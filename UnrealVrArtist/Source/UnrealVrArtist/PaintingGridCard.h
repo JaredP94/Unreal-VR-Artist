bb744272a1212ce2b16a1a2f86646469d8b07539
JaredP94 <jaredping@yahoo.com>
2020-04-05 18:56:58 +0200

    Add binding support for titles of cards

---
+++ UnrealVrArtist/Source/UnrealVrArtist/PaintingGridCard.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "Components/TextBlock.h"
+
+#include "PaintingGridCard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALVRARTIST_API UPaintingGridCard : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (BindWidget))
+	UTextBlock* SlotName;
+
+};
