a071ab61ff0927280bb00220ba1b7c1bf6282b77
JaredP94 <jaredping@yahoo.com>
2020-04-06 22:06:37 +0200

    Load blank level when selecting save slot

--- UnrealVrArtist/Source/UnrealVrArtist/PaintingGridCard.h
+++ UnrealVrArtist/Source/UnrealVrArtist/PaintingGridCard.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 
+#include "Components/Button.h"
 #include "Components/TextBlock.h"
 
 #include "PaintingGridCard.generated.h"
@@ -24,4 +25,13 @@ protected:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (BindWidget))
 	UTextBlock* SlotName;
 
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (BindWidget))
+	UButton* CardButton;
+
+private:
+	UFUNCTION()
+	void CardButtonClicked();
+
+	// State
+	FString PaintingName;
 };
